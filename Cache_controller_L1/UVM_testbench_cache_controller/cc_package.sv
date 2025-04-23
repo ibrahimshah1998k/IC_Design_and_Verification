18bb5dd32b2dc71a556d5c9bc802e7cfc507e825
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2025-04-23 19:25:00 +0500

    Create cc_package.sv

---
+++ Cache_controller_L1/UVM_testbench_cache_controller/cc_package.sv
@@ -0,0 +1,14 @@
+package my_testbench_pkg;
+import uvm_pkg::*; 
+`include "uvm_macros.svh"
+
+`include "cc_seq_item.sv"
+`include "cc_sequencer.sv"
+`include "cc_sequence.sv"
+`include "cc_driver.sv"
+`include "cc_monitor.sv"
+`include "cc_agent.sv"
+`include "cc_scoreboard.sv"
+`include "cc_env.sv"
+`include "cc_base_test.sv"
+endpackage
