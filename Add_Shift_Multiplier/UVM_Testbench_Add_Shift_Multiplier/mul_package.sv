59c375e3dc228a0b68e73c8a9ecae8c9a045bd74
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2025-04-23 18:32:16 +0500

    Create mul_package.sv

---
+++ Add_Shift_Multiplier/UVM_Testbench_Add_Shift_Multiplier/mul_package.sv
@@ -0,0 +1,15 @@
+package my_testbench_pkg;
+import uvm_pkg::*; 
+`include "uvm_macros.svh"
+
+`include "mul_seq_item.sv"
+`include "mul_sequencer.sv"
+`include "mul_sequence.sv"
+`include "mul_driver.sv"
+`include "mul_monitor.sv"
+`include "mul_agent.sv"
+`include "mul_scoreboard.sv"
+`include "mul_coverage.sv"
+`include "mul_env.sv"
+`include "mul_base_test.sv"
+endpackage
