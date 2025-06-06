803fa0988d872d8ac0c0ca16e8dea67ecb6849b9
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2025-06-06 01:44:33 -0700

    Create ahb_package.sv
    ahb package

---
+++ AMBA_Protocols/AHB_Protocol/UVM_TB_AHB_Protocol/ahb_package.sv
@@ -0,0 +1,14 @@
+package my_testbench_pkg;
+import uvm_pkg::*; 
+`include "uvm_macros.svh"
+
+`include "ahb_seq_item.sv"
+`include "ahb_sequencer.sv"
+`include "ahb_sequence.sv"
+`include "ahb_driver.sv"
+`include "ahb_monitor.sv"
+`include "ahb_agent.sv"
+`include "ahb_scoreboard.sv"
+`include "ahb_env.sv"
+`include "ahb_base_test.sv"
+endpackage
