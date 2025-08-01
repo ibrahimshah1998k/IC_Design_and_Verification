50509386824b0ab877be19bd85eba2202abc39ba
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2025-08-01 21:21:11 +0500

    Create APB_package.sv
    added code

---
+++ AMBA_Protocols/APB_Protocol/UVM_TB_APB/APB_package.sv
@@ -0,0 +1,14 @@
+package my_testbench_pkg;
+import uvm_pkg::*; 
+`include "uvm_macros.svh"
+
+`include "APB_seq_item.sv"
+`include "APB_sequencer.sv"
+`include "APB_sequence.sv"
+`include "APB_driver.sv"
+`include "APB_monitor.sv"
+`include "APB_agent.sv"
+`include "APB_scoreboard.sv"
+`include "APB_env.sv"
+`include "APB_base_test.sv"
+endpackage
